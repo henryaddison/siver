c89cf60e47eacb4ed30ec2d546c4b4fb2513d957
Henry Addison <hja22@hja22.co.uk>
2012-06-18 17:11:15 +0100

    got a boat that bounces up and down a straight river

--- src/siver/BoatAgent.java
+++ src/siver/BoatAgent.java
@@ -1,13 +1,52 @@
 package siver;
 
 import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.space.SpatialMath;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
 
 public class BoatAgent {
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+	private boolean upstream;
+	public BoatAgent(ContinuousSpace<Object> space, Grid<Object> grid) {
+		this.space = space;
+		this.grid = grid;
+		this.upstream = false;
+	}
+	
 	// Schedule the step method for agents.  The method is scheduled starting at 
 	// tick one with an interval of 1 tick.  Specifically, the step starts at 1, and
 	// and recurs at 2,3,4,...etc
 	@ScheduledMethod(start = 1, interval = 1, shuffle=true)
 	public void step() {
-		// Override by subclasses
+		GridPoint myLocation = grid.getLocation(this);
+		if(!upstream && myLocation.getY() < 48) {
+			GridPoint pt = new GridPoint(15,49);
+			moveToward(pt);
+		} else if(!upstream && myLocation.getY() >= 48) {
+			spin();
+		} else if(upstream && myLocation.getY() <= 1) {
+			spin();
+		} else if(upstream && myLocation.getY() > 1){
+			GridPoint pt = new GridPoint(15,0);
+			moveToward(pt);
+		}
+	}
+
+
+	private void spin() {
+		upstream = !upstream;
+	}
+	
+	private void moveToward(GridPoint pt) {
+		NdPoint myPoint  = space.getLocation(this);
+		NdPoint otherPoint = new NdPoint(pt.getX(), pt.getY());
+		double angle = SpatialMath.calcAngleFor2DMovement(space, myPoint, otherPoint);
+		space.moveByVector(this, 1, angle, 0);
+		myPoint = space.getLocation(this);
+		grid.moveTo(this, (int)myPoint.getX(), (int)myPoint.getY());
 	}
 }
--- src/siver/SiverContextCreator.java
+++ src/siver/SiverContextCreator.java
@@ -7,7 +7,10 @@ import repast.simphony.context.Context;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.continuous.SimpleCartesianAdder;
+import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridBuilderParameters;
 import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.valueLayer.GridValueLayer;
@@ -33,7 +36,7 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 		// and the grid parameters.  Grid parameters include the border specification,
 		// random adder for populating the grid with agents, boolean for multiple occupancy,
 		// and the dimensions of the grid.
-		GridFactoryFinder.createGridFactory(null).createGrid("Simple Grid", context,
+		Grid<Object> grid = GridFactoryFinder.createGridFactory(null).createGrid("Simple Grid", context,
 				new GridBuilderParameters<Object>(new repast.simphony.space.grid.StrictBorders(),
 						new SimpleGridAdder<Object>(), true, xdim, ydim));
 
@@ -42,9 +45,9 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 		// the context in which to place the space, border specification,
 		// random adder for populating the grid with agents,
 		// and the dimensions of the grid.
-		ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null)
+		ContinuousSpace<Object> space = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null)
 		.createContinuousSpace("Continuous Space", context, new SimpleCartesianAdder<Object>(),
-				new repast.simphony.space.continuous.StrictBorders(), xdim, ydim, 1);
+				new repast.simphony.space.continuous.StrictBorders(), xdim, ydim);
 
 		// Create a new 2D value layer to store the state of the river grid.  This is
 		// only used for visualization since it's faster to draw the value layer
@@ -59,6 +62,12 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 				vl.set(1, x, y);
 			}
 		}
+		
+		BoatAgent boat = new BoatAgent(space, grid);
+		context.add(boat);
+		space.moveTo(boat, 15, 0);
+		NdPoint pt = space.getLocation(boat);
+		grid.moveTo(boat, (int)pt.getX(), (int)pt.getY());
 		
 		return context;
 	}
