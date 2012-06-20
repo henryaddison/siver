90d569fd4721421879a257f81bfeae7bf3031031
Henry Addison <hja22@hja22.co.uk>
2012-06-20 11:10:58 +0100

    added first test class

---
+++ test/siver/LandmarkTest.java
@@ -0,0 +1,49 @@
+package siver;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LandmarkTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLandmark() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+}
