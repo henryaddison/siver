e8487d5f2dbcef3a8a158caf82e7c71abb3a53ac
Henry Addison <hja22@hja22.co.uk>
2012-07-02 20:34:33 +0100

    added some comments to the LaneEdge and Creator clases

--- src/siver/river/lane/LaneEdge.java
+++ src/siver/river/lane/LaneEdge.java
@@ -6,12 +6,20 @@ package siver.river.lane;
 import repast.simphony.space.graph.RepastEdge;
 
 /**
+ * A class to represent the edges in a Lane.
+ * These are travelled by boats whose coxes will react when they reach each node.
+ * For simplicity they are directed (going upstream get predecessor node while 
+ * going downsteam get the successor node).
+ * The weight is set to be the distance between nodes by default.
+ *  
  * @author henryaddison
  *
  */
 public class LaneEdge<T extends LaneNode> extends RepastEdge<T> {
 	public LaneEdge(T source, T destination) {
+		//shall assume that LaneEdges are directed by default
 		super(source, destination, true);
+		//weight of an edge should be the distance between the two nodes
 		double w = source.distance(destination);
 		setWeight(w);
 	}
--- src/siver/river/lane/LaneEdgeCreator.java
+++ src/siver/river/lane/LaneEdgeCreator.java
@@ -6,6 +6,11 @@ package siver.river.lane;
 import repast.simphony.space.graph.EdgeCreator;
 
 /**
+ * Class used by Repast's NetworkBuilder.
+ * When building a network to represent a lane using LaneNodes connected by directed
+ * LaneEdges and overriding the weight to be the distance between the nodes 
+ * (hence we the isDirected and weight arguments are dropped on the floor by createEdge). 
+ * 
  * @author henryaddison
  *
  */
@@ -18,6 +23,7 @@ public class LaneEdgeCreator<T extends LaneNode> implements EdgeCreator<LaneEdge
 
 	@Override
 	public LaneEdge<T> createEdge(T source, T target, boolean isDirected, double weight) {
+		//LaneEdgeCreator is only used for directed graphs where the weight is used to determine the distance between nodes.
 		return new LaneEdge<T>(source, target);
 	}
 
