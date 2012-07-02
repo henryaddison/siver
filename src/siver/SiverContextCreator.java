37b7a6d4705a83c6359d6c5b32e524d105de0802
Henry Addison <hja22@hja22.co.uk>
2012-07-02 13:41:17 +0100

    broken code pre supervisor meeting

--- src/siver/SiverContextCreator.java
+++ src/siver/SiverContextCreator.java
@@ -37,8 +37,6 @@ import siver.river.RiverFactory;
 public class SiverContextCreator implements ContextBuilder<Object> {
 	
 	private static BoatHouse boatHouse;
-	private static River river;
-	private static Network<LaneNode> laneNetwork;
 	
 	/* (non-Javadoc)
 	 * @see repast.simphony.dataLoader.ContextBuilder#build(repast.simphony.context.Context)
@@ -48,7 +46,7 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 		context.setId("siver");
 		
 		int xdim = 1500;   // The x dimension of the physical space
-		int ydim = 1500;   // The y dimension of the physical space
+		int ydim = 300;   // The y dimension of the physical space
 
 		// Create a new 2D continuous space to model the physical space on which the sheep
 		// and wolves will move.  The inputs to the Space Factory include the space name, 
@@ -59,58 +57,10 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 		.createContinuousSpace("Continuous Space", context, new SimpleCartesianAdder<Object>(),
 				new repast.simphony.space.continuous.StrictBorders(), xdim, ydim);
 		
-		river = RiverFactory.Test();
-		context.add(river);
-		space.moveTo(river, 0,0);
-
-		boatHouse = new BoatHouse(new Point2D.Double(0, 15), river);
-		context.add(boatHouse);
-		space.moveTo(boatHouse, 0, 15);
+		RiverFactory.Test(context, space);
 		
-		Context<LaneNode> lane_context = new LaneContext();
-		context.addSubContext(lane_context);
 		
-		NetworkBuilder<LaneNode> builder = new NetworkBuilder<LaneNode>("Middle Lane",
-				lane_context, true);
-		builder.setEdgeCreator(new LaneEdgeCreator<LaneNode>());
-		laneNetwork = builder.buildNetwork();
-		
-		LaneNode current = boatHouse;
-		
-		for(int i = 10; i <= 985; i+=10) {
-			LaneNode next = new LaneNode(i,15);
-			laneNetwork.addEdge(current, next, next.distance(current));
-			current = next;
-		}
-		
-		for(int i = 15; i <= 265; i+=10) {
-			LaneNode next = new LaneNode(985, i);
-			laneNetwork.addEdge(current, next, next.distance(current));
-			current = next;
-		}
-		
-		for(int i = 985; i< 1500; i+=10) {
-			LaneNode next = new LaneNode(i, 265);
-			laneNetwork.addEdge(current, next, next.distance(current));
-			current = next;
-		}
-		
-		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
-		//Specify that the action should start at tick 1 and execute every other tick
-		ScheduleParameters params = ScheduleParameters.createOneTime(1);
-
-		//Schedule the boathouse to launch a boat on the first tick only for now
-		schedule.schedule(params, boatHouse, "launch");
 		
 		return context;
 	}
-	
-	public static BoatHouse getBoatHouse() {
-		return boatHouse;
-	}
-	
-	public static Network<LaneNode> getMiddleLane() {
-		return laneNetwork;
-	}
-	
 }
--- src/siver/river/RiverFactory.java
+++ src/siver/river/RiverFactory.java
@@ -2,6 +2,17 @@ package siver.river;
 
 import java.awt.geom.Point2D;
 
+import lane.LaneEdgeCreator;
+import lane.LaneNode;
+import repast.simphony.context.Context;
+import repast.simphony.context.space.graph.NetworkBuilder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ISchedule;
+import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.space.continuous.ContinuousSpace;
+import siver.BoatHouse;
+import siver.LaneContext;
+
 /**
  * Factory for making complete River objects.
  * 
@@ -14,8 +25,25 @@ public class RiverFactory {
 	 * 
 	 * @return completed River object based on the test coordinates
 	 */
-	public static River Test() {
+	public static River Test(Context<Object> context, ContinuousSpace<Object> space) {
 		River river = new River();
+		
+		Context<LaneNode> lane_context = new LaneContext();
+		context.addSubContext(lane_context);
+		
+		context.add(river);
+		space.moveTo(river, 0,0);
+		
+		BoatHouse boatHouse = new BoatHouse(new Point2D.Double(0, 15), river);
+		context.add(boatHouse);
+		space.moveTo(boatHouse, 0, 15);
+		
+		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
+		//Specify that the action should start at tick 1 and execute every other tick
+		ScheduleParameters params = ScheduleParameters.createOneTime(1);
+
+		//Schedule the boathouse to launch a boat on the first tick only for now
+		schedule.schedule(params, boatHouse, "launch");
 		
 		river.add(0,0);
 		river.add(1000, 0);
@@ -27,6 +55,41 @@ public class RiverFactory {
 		river.add(0,30);
 		
 		river.complete();
+		
+		NetworkBuilder<LaneNode> builder = new NetworkBuilder<LaneNode>("Middle Lane",
+				lane_context, true);
+		builder.setEdgeCreator(new LaneEdgeCreator<LaneNode>());
+		middleLaneNetwork = builder.buildNetwork();
+		
+		builder = new NetworkBuilder<LaneNode>("Downstream Lane",
+				lane_context, true);
+		builder.setEdgeCreator(new LaneEdgeCreator<LaneNode>());
+		downstreamLaneNetwork = builder.buildNetwork();
+		
+		builder = new NetworkBuilder<LaneNode>("Upstream Lane",
+				lane_context, true);
+		builder.setEdgeCreator(new LaneEdgeCreator<LaneNode>());
+		upstreamLaneNetwork = builder.buildNetwork();
+		
+		LaneNode current = boatHouse;
+		
+		for(int i = 10; i <= 985; i+=10) {
+			LaneNode next = new LaneNode(i,15);
+			middleLaneNetwork.addEdge(current, next, next.distance(current));
+			current = next;
+		}
+		
+		for(int i = 15; i <= 265; i+=10) {
+			LaneNode next = new LaneNode(985, i);
+			middleLaneNetwork.addEdge(current, next, next.distance(current));
+			current = next;
+		}
+		
+		for(int i = 985; i< 1500; i+=10) {
+			LaneNode next = new LaneNode(i, 265);
+			middleLaneNetwork.addEdge(current, next, next.distance(current));
+			current = next;
+		}
 		
 		return river;
 	}
