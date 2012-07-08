8645674d7a04c4da37a67edeb8323bad79a86b0f
Henry Addison <hja22@hja22.co.uk>
2012-07-08 13:08:41 +0100

    added tests for LaneEdge class

--- test/siver/AllTests.java
+++ test/siver/AllTests.java
@@ -5,11 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import siver.river.RiverTest;
+import siver.river.lane.LaneEdgeTest;
 import siver.river.lane.LaneNodeTest;
 import siver.river.lane.LaneTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LaneTest.class, RiverTest.class, LaneNodeTest.class })
+@SuiteClasses({ LaneTest.class, RiverTest.class, LaneNodeTest.class, LaneEdgeTest.class})
 public class AllTests {
 
 }
---
+++ test/siver/river/lane/LaneEdgeTest.java
@@ -0,0 +1,37 @@
+package siver.river.lane;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LaneEdgeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLaneEdge() {
+		LaneNode s = new LaneNode(0,0);
+		LaneNode d = new LaneNode(3,4);
+		LaneEdge<LaneNode> e = new LaneEdge<LaneNode>(s,d);
+		assertEquals(5.0, e.getWeight(), 1E-5);
+	}
+	
+}
