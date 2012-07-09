4af6bea6425f693eb71c39f5e5a7a8030ca343e3
Henry Addison <hja22@hja22.co.uk>
2012-07-09 16:09:55 +0100

    using the shared behaviour of LanedTest

--- test/siver/river/RiverTest.java
+++ test/siver/river/RiverTest.java
@@ -10,37 +10,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import siver.LanedTest;
 import siver.context.LaneContext;
 import siver.river.River;
 import siver.river.lane.Lane;
 
-public class RiverTest {
-	private static Lane up, down, mid;
+public class RiverTest extends LanedTest {
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		up = new Lane(new LaneContext(), "Upstream Test Lane");
-		down = new Lane(new LaneContext(), "Downstream Test Lane");
-		mid = new Lane(new LaneContext(), "Middle Test Lane");
-		
-		up.start(new Point2D.Double(0,50));
-		mid.start(new Point2D.Double(0,30));
-		down.start(new Point2D.Double(0,10));
-		
-		up.extend(0);
-		up.extend(0);
-		up.extend(0);
-		
-		mid.extend(0);
-		mid.extend(0);
-		mid.extend(0);
-		
-		down.extend(0);
-		down.extend(0);
-		down.extend(0);
-		
-		up.complete();
-		mid.complete();
-		down.complete();
+		createLanes();
 	}
 
 	@AfterClass
