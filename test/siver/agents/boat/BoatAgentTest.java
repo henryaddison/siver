66c660ca85e805bdbe0e09d79ece57e9da92adaf
Henry Addison <hja22@hja22.co.uk>
2012-07-11 11:46:38 +0100

    first couple of tests for BoatAgent

--- test/siver/agents/boat/BoatAgentTest.java
+++ test/siver/agents/boat/BoatAgentTest.java
@@ -1,15 +1,28 @@
 package siver.agents.boat;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
+import java.awt.geom.Point2D;
+
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import repast.simphony.context.Context;
+import repast.simphony.space.continuous.ContinuousSpace;
+import siver.river.River;
+
 public class BoatAgentTest {
 
+	private Context<Object> mockContext;
+	private ContinuousSpace<Object> mockSpace;
+	private River mockRiver;
+	private BoatAgent boat;
+	private CoxAgent mockCox;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -20,6 +33,11 @@ public class BoatAgentTest {
 
 	@Before
 	public void setUp() throws Exception {
+		mockContext = EasyMock.createMock(Context.class);
+		mockSpace = EasyMock.createMock(ContinuousSpace.class);
+		mockRiver = EasyMock.createMock(River.class);
+		mockCox = EasyMock.createMock(CoxAgent.class);
+		boat = new BoatAgent(mockRiver, mockContext, mockSpace);
 	}
 
 	@After
@@ -28,16 +46,34 @@ public class BoatAgentTest {
 
 	@Test
 	public void testBoatAgent() {
-		fail("Not yet implemented");
+		replay(mockContext);
+		replay(mockSpace);
+		replay(mockRiver);
+		assertNotNull(boat);
+		assertTrue(boat instanceof BoatAgent);
+		verify(mockContext);
+		verify(mockSpace);
+		verify(mockRiver);
 	}
 
 	@Test
 	public void testLaunch() {
-		fail("Not yet implemented");
+		expect(mockSpace.moveTo(boat, 0,0)).andReturn(true).times(1);
+		expect(mockContext.add(anyObject(BoatCorner.class))).andReturn(true).times(4);
+		replay(mockContext);
+		replay(mockSpace);
+		replay(mockRiver);
+		replay(mockCox);
+		
+		boat.launch(mockCox, new Point2D.Double(0,0));
+		verify(mockContext);
+		verify(mockSpace);
+		verify(mockRiver);
+		verify(mockCox);
 	}
 
 	@Test
-	public void testStep() {
+	public void testLand() {
 		fail("Not yet implemented");
 	}
 
@@ -47,12 +83,12 @@ public class BoatAgentTest {
 	}
 
 	@Test
-	public void testSetAngle() {
+	public void testGetAngle() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetAngle() {
+	public void testSetAngle() {
 		fail("Not yet implemented");
 	}
 
@@ -62,7 +98,17 @@ public class BoatAgentTest {
 	}
 
 	@Test
+	public void testSetSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
 	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSpace() {
 		fail("Not yet implemented");
 	}
 
