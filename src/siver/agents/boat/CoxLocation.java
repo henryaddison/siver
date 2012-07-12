6ea7b3cb24d824488e82fd4f295f18bf6dcd4c99
Henry Addison <hja22@hja22.co.uk>
2012-07-12 15:18:25 +0100

    CoxLocation now using lane attached to node

--- src/siver/agents/boat/CoxLocation.java
+++ src/siver/agents/boat/CoxLocation.java
@@ -5,23 +5,24 @@ import siver.river.lane.LaneEdge;
 import siver.river.lane.LaneNode;
 
 public class CoxLocation {
-	// The lane the cox is in.
-	private Lane lane;
-	
 	// The part of a the lane the cox is currently travelling on.
 	private LaneEdge<LaneNode> current_edge;
 	
 	// How much of the current edge is left before the cox reaches the end and then next node.
 	private double till_edge_end;
 	
-	public CoxLocation(Lane l, LaneEdge<LaneNode> e, double tee) {
-		lane = l;
+	public CoxLocation(LaneEdge<LaneNode> e, double tee) {
 		current_edge = e;
 		till_edge_end = tee;
 	}
 	
+	public CoxLocation(LaneEdge<LaneNode> e) {
+		current_edge = e;
+		till_edge_end = e.getWeight();
+	}
+	
 	public Lane getLane() {
-		return lane;
+		return current_edge.getSource().getLane();
 	}
 	
 	public LaneEdge<LaneNode> getEdge() {
--- test/siver/agents/boat/CoxLocationTest.java
+++ test/siver/agents/boat/CoxLocationTest.java
@@ -32,12 +32,23 @@ public class CoxLocationTest {
 	@Test
 	public void testCoxLocation() {
 		Lane exp_lane = new Lane(new LaneContext(), "Test Lane");
-		LaneEdge<LaneNode> exp_edge = new LaneEdge<LaneNode>(new LaneNode(0,30), new LaneNode(20,30));
-		CoxLocation cl = new CoxLocation(exp_lane, exp_edge, 20);
+		LaneEdge<LaneNode> exp_edge = new LaneEdge<LaneNode>(new LaneNode(0,30, exp_lane), new LaneNode(20,30, exp_lane));
+		CoxLocation cl = new CoxLocation(exp_edge, 20);
 		assertTrue(cl instanceof CoxLocation);
 		assertEquals(exp_lane, cl.getLane());
 		assertEquals(exp_edge, cl.getEdge());
 		assertEquals(20, cl.getTillEdgeEnd(), 1E-5);
 	}
-
+	
+	@Test
+	public void testCoxLocationWithoutDistance() {
+		Lane exp_lane = new Lane(new LaneContext(), "Test Lane");
+		LaneEdge<LaneNode> exp_edge = new LaneEdge<LaneNode>(new LaneNode(0,30, exp_lane), new LaneNode(20,30, exp_lane));
+		CoxLocation cl = new CoxLocation(exp_edge);
+		assertTrue(cl instanceof CoxLocation);
+		assertEquals(exp_lane, cl.getLane());
+		assertEquals(exp_edge, cl.getEdge());
+		assertEquals(20, cl.getTillEdgeEnd(), 1E-5);
+	}
+	
 }
