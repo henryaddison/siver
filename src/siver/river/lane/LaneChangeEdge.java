3bbc6301eab9eea790cc6da6f8f0541e56cc5860
Henry Addison <hja22@hja22.co.uk>
2012-07-20 17:49:04 +0100

    first stab at tracking the proper edges occupied as a cox traverses a lane changing edge

--- src/siver/river/lane/LaneChangeEdge.java
+++ src/siver/river/lane/LaneChangeEdge.java
@@ -1,11 +1,13 @@
 package siver.river.lane;
 
+import repast.simphony.space.graph.RepastEdge;
 import siver.agents.boat.CoxAgent;
 
 public class LaneChangeEdge<T extends LaneNode> extends LaneEdge<T> {
 	private Lane startLane, destinationLane;
-	private LaneEdge<LaneNode> occupyingInStartLane;
-	private LaneEdge<LaneNode> occupyingInDestinationLane;
+	//need to keep track of which proper edges are currently being occupied while a boat traverses this lane
+	//fortunately can only have one boat on an edge like this at a time so 
+	private LaneEdge<LaneNode> occupyingInDestinationLane, occupyingInStartLane;
 	
 	public LaneChangeEdge(T source, T destination, Lane sLane) {
 		super(source, destination);
@@ -15,20 +17,44 @@ public class LaneChangeEdge<T extends LaneNode> extends LaneEdge<T> {
 	
 	@Override
 	public void addCox(CoxAgent cox) {
-		occupyingInStartLane = cox.getLocation().getEdge();
 		occupyingInDestinationLane = destinationLane.edgeNearest(cox.getBoat().getLocation());
 		occupyingInDestinationLane.addCox(cox);
 	}
 	
 	@Override
 	public void removeCox(CoxAgent cox) {
-		occupyingInStartLane.removeCox(cox);
 		occupyingInDestinationLane.removeCox(cox);
+		occupyingInStartLane.removeCox(cox);
+		
+		destinationLane.getNet().removeEdge((RepastEdge<LaneNode>) this);
+		
+		LaneNode endPoint;
+		endPoint = this.getSource();
+		if(endPoint.isTemporary())	destinationLane.getContext().remove(endPoint);
+		endPoint = this.getTarget();
+		if(endPoint.isTemporary())	destinationLane.getContext().remove(endPoint);
 	}
 	
 	
+	//when a boat moves along an edge like this we need to keep track of which real edges are actually being occupied by this boat
+	//we use this call back to update the edges occupied by this cox
+	//since there's only one boat on this 
 	@Override
 	public void coxMoved(CoxAgent cox) {
+		LaneEdge<LaneNode> newOccupyingInDestinationLane = destinationLane.edgeNearest(cox.getBoat().getLocation());
+		if(newOccupyingInDestinationLane != occupyingInDestinationLane) {
+			occupyingInDestinationLane.removeCox(cox);
+			newOccupyingInDestinationLane.addCox(cox);
+			occupyingInDestinationLane = newOccupyingInDestinationLane;
+		}
+		
+		LaneEdge<LaneNode> newOccupyingInStartLane = startLane.edgeNearest(cox.getBoat().getLocation());
+		if(newOccupyingInStartLane != occupyingInStartLane) {
+			occupyingInStartLane.removeCox(cox);
+			newOccupyingInStartLane.addCox(cox);
+			occupyingInStartLane = newOccupyingInStartLane;
+		}
+		
 		
 	}
 }
