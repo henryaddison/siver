a8b0244d4323adb86109513b80fab26c547060c1
Henry Addison <hja22@hja22.co.uk>
2012-07-28 13:36:24 +0100

    implemented tests for LaneChangeEdge

--- test/siver/river/lane/LaneChangeEdgeTest.java
+++ test/siver/river/lane/LaneChangeEdgeTest.java
@@ -1,5 +1,6 @@
 package siver.river.lane;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -8,9 +9,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import repast.simphony.space.graph.Network;
+import repast.simphony.space.graph.RepastEdge;
+import siver.agents.boat.CoxAgent;
+import siver.agents.boat.CoxLocation;
+import siver.context.LaneContext;
+
 public class LaneChangeEdgeTest extends EdgeTester {
 	private LaneChangeEdge e;
-	
+	private LaneEdge sLEdge, dLEdge;
+	private LaneContext laneContext;
+	private Network<LaneNode> mockNet;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -24,9 +33,20 @@ public class LaneChangeEdgeTest extends EdgeTester {
 	public void setUp() throws Exception {
 		super.setUp();
 		
-		LaneNode s = new LaneNode(0,0,null);
-		LaneNode d = new LaneNode(3,4,null);
-		e = new LaneChangeEdge(s,d, null, null);
+		sLEdge = createMock(LaneEdge.class);
+		dLEdge = createMock(LaneEdge.class);
+		Lane mockLane = createMock(Lane.class);
+		mockNet = createMock(Network.class);
+		laneContext = new LaneContext("Test");
+		
+		expect(mockLane.getNet()).andStubReturn(mockNet);
+		expect(mockLane.getContext()).andStubReturn(laneContext);
+		
+		replay(mockLane);
+		
+		LaneNode s = new LaneNode(0,0,mockLane);
+		LaneNode d = new LaneNode(3,4,mockLane);
+		e = new LaneChangeEdge(s,d, sLEdge, dLEdge);
 	}
 
 	@After
@@ -35,17 +55,43 @@ public class LaneChangeEdgeTest extends EdgeTester {
 
 	@Test
 	public void testAddCox() {
-		fail("Not yet implemented");
+		CoxAgent cox = new CoxAgent();
+		
+		sLEdge.addCox(cox);
+		expectLastCall().once();
+		
+		dLEdge.addCox(cox);
+		expectLastCall().once();
+		
+		replay(sLEdge, dLEdge);
+		
+		e.addCox(cox);
+		
+		verify(sLEdge, dLEdge);
 	}
 
 	@Test
 	public void testRemoveCox() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLaneChangeEdge() {
-		fail("Not yet implemented");
+		CoxAgent cox = createMock(CoxAgent.class);
+		CoxLocation mockLocation = createMock(CoxLocation.class);
+		
+		expect(cox.getLocation()).andStubReturn(mockLocation);
+		expect(mockLocation.headingUpstream()).andReturn(false).once();
+		
+		mockNet.removeEdge((RepastEdge<LaneNode>) e);
+		expectLastCall().once();
+		
+		sLEdge.removeCox(cox);
+		expectLastCall().once();
+		
+		dLEdge.removeCox(cox);
+		expectLastCall().once();
+		
+		replay(sLEdge, dLEdge, mockNet, cox, mockLocation);
+		
+		e.removeCox(cox);
+		
+		verify(sLEdge, dLEdge, mockLocation);
 	}
 	
 	@Test
