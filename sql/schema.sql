d60f1c5623805a38fafa68e9cfd54c08922c4715
Henry Addison <hja22@hja22.co.uk>
2012-08-06 19:15:06 +0100

    added some more data to schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,9 +15,12 @@ CREATE TABLE scheduled_launches (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	schedule_id INT NOT NULL,
 	desired_gear INT NOT NULL,
+	speed_multiplier DOUBLE NOT NULL DEFAULT 0.5,
+	distance_to_cover DOUBLE NOT NULL,
 	launch_tick INT NOT NULL,
 	brain_type VARCHAR(255) NOT NULL DEFAULT "BasicBrain",
-	CONSTRAINT launch_schedule_fk FOREIGN KEY (schedule_id) REFERENCES schedules(id)
+	CONSTRAINT launch_schedule_fk FOREIGN KEY (schedule_id) REFERENCES schedules(id),
+	CONSTRAINT tick_unique_key UNIQUE KEY (schedule_id, launch_tick)
 ) ENGINE=innodb;
 
 CREATE TABLE experiment_runs (
@@ -35,10 +38,14 @@ CREATE TABLE experiment_runs (
 CREATE TABLE boat_records (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	scheduled_launch_id INT DEFAULT NULL,
+	experiment_run_id INT NOT NULL,
 	launch_tick INT NOT NULL,
 	land_tick INT DEFAULT NULL,
 	desired_gear INT NOT NULL,
+	speed_multiplier DOUBLE NOT NULL DEFAULT 0.5,
+	distance_covered DOUBLE NOT NULL,
 	aggregate_gear_difference INT NOT NULL,
 	brain_type VARCHAR(255) NOT NULL DEFAULT "BasicBrain",
-	CONSTRAINT boat_record_launch_fk FOREIGN KEY (scheduled_launch_id) REFERENCES scheduled_launches(id)
+	CONSTRAINT boat_record_launch_fk FOREIGN KEY (scheduled_launch_id) REFERENCES scheduled_launches(id),
+	CONSTRAINT boat_record_experiment_run_fk FOREIGN KEY (experiment_run_id) REFERENCES experiment_runs(id)
 ) ENGINE=innodb;
