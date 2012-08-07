863d54a0f92cf6c9573247dffa03a0f1724cc5f0
Henry Addison <hja22@hja22.co.uk>
2012-08-07 17:32:51 +0100

    brain type is now  an argument for schedule geneartion script

--- schedule_generation/create_schedule.rb
+++ schedule_generation/create_schedule.rb
@@ -13,6 +13,7 @@ require './scheduled_launch'
 schedule_name = nil
 experiment_name = nil
 random_seed = rand(2**31)
+brain_type = "BasicBrain"
 
 optparse = OptionParser.new do|opts|
   opts.on( '-s', '--schedule-name SNAME', "Schedule name" ) do |sn|
@@ -23,6 +24,9 @@ optparse = OptionParser.new do|opts|
     random_seed = rs
   end
   
+  opts.on('-b', '--brain-type BRAIN_TYPE', "Brain class name") do |bt|
+    brain_type = bt
+  end
 end
 
 optparse.parse!
@@ -39,6 +43,6 @@ Schedule.transaction do
   experiment = Experiment.create!(:schedule => schedule, :random_seed => random_seed)
   
   100.times do |i|
-    ScheduledLaunch.create!(:schedule => schedule, :desired_gear => rand(10)+1, :speed_multiplier => 0.5, :distance_to_cover => 4000, :launch_tick => i*10, :brain_type => "BasicBrain")
+    ScheduledLaunch.create!(:schedule => schedule, :desired_gear => rand(10)+1, :speed_multiplier => 0.5, :distance_to_cover => 4000, :launch_tick => i*60, :brain_type => brain_type)
   end
 end
