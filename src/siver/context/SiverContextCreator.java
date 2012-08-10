0c3e35c8b47ff53eaf11ae91400cb7f47b3a0deb
Henry Addison <hja22@hja22.co.uk>
2012-08-10 11:37:34 +0100

    Cleaned up SiverContextCreator: moved the global variables with their getters and setters to bottom for readibility; matched names of DataCollection start and stop methods for constency and made them private as nothing else should call them.

--- src/siver/context/SiverContextCreator.java
+++ src/siver/context/SiverContextCreator.java
@@ -22,7 +22,63 @@ import siver.ui.UserPanel;
  * @author hja11
  *
  */
-public class SiverContextCreator implements ContextBuilder<Object> {
+public class SiverContextCreator implements ContextBuilder<Object> {	
+	/* (non-Javadoc)
+	 * @see repast.simphony.dataLoader.ContextBuilder#build(repast.simphony.context.Context)
+	 */
+	@Override
+	public Context build(Context<Object> context) {
+		context.setId("siver");
+		mainContext = context;
+		
+		int xdim = 2200;   // The x dimension of the physical space
+		int ydim = 1000;   // The y dimension of the physical space
+		
+		space = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null)
+		.createContinuousSpace("Continuous Space", context, new SimpleCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.StrictBorders(), xdim, ydim);
+		
+		river = RiverFactory.Cam(context, space);
+		
+		boatHouse = new BoatHouse(river, context, space);
+		context.add(boatHouse);
+		space.moveTo(boatHouse, 0, 20);
+		
+		initializeDataCollection();
+		
+		return context;
+	}
+	
+	public static void stopSim() {
+		RunEnvironment.getInstance().getCurrentSchedule().setFinishing(true);
+	}
+	
+	private void initializeDataCollection() {
+		InprogressExperiment.start();
+		
+		//schedule a method to run when the simulation ends so we can flush all collected data to database
+		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
+		ScheduleParameters params = ScheduleParameters.createOneTime(ScheduleParameters.END);
+		schedule.schedule(params, this, "stopDataCollection");
+	}
+	
+	private void stopDataCollection() {
+		InprogressExperiment.end();
+	}
+	
+	//helpers to get the current tick count
+		public static int getTickCount() {
+			RunEnvironment re = RunEnvironment.getInstance();
+			
+			if(re != null && re.getCurrentSchedule() != null) {
+				return (int) re.getCurrentSchedule().getTickCount();
+			} else {
+				return 0;
+			}
+		}
+	
+	//Keep track of the important global objects in the simulation (the context and space for the 2D visualization, and the river and the boathouse
+	//have setters so they may be overridden in unit tests (e.g. by mock objects)
 	private static Context<Object> mainContext;
 	public static Context<Object> getContext() {
 		return mainContext;
@@ -52,60 +108,5 @@ public class SiverContextCreator implements ContextBuilder<Object> {
 	}
 	public static void setBoatHouse(BoatHouse bh) {
 		boatHouse = bh;
-	}
-	
-	
-	public static int getTickCount() {
-		RunEnvironment re = RunEnvironment.getInstance();
-		
-		if(re != null && re.getCurrentSchedule() != null) {
-			return (int) re.getCurrentSchedule().getTickCount();
-		} else {
-			return 0;
-		}
-		
-	}
-	
-	/* (non-Javadoc)
-	 * @see repast.simphony.dataLoader.ContextBuilder#build(repast.simphony.context.Context)
-	 */
-	@Override
-	public Context build(Context<Object> context) {
-		context.setId("siver");
-		mainContext = context;
-		
-		int xdim = 2200;   // The x dimension of the physical space
-		int ydim = 1000;   // The y dimension of the physical space
-		
-		space = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null)
-		.createContinuousSpace("Continuous Space", context, new SimpleCartesianAdder<Object>(),
-				new repast.simphony.space.continuous.StrictBorders(), xdim, ydim);
-		
-		river = RiverFactory.Cam(context, space);
-		
-		boatHouse = new BoatHouse(river, context, space);
-		context.add(boatHouse);
-		space.moveTo(boatHouse, 0, 20);
-		
-		initializeDataCollection();
-		
-		return context;
-	}
-	
-	public void initializeDataCollection() {
-		InprogressExperiment.start();
-		
-		//schedule a method to run when the simulation ends so we can flush all collected data to database
-		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
-		ScheduleParameters params = ScheduleParameters.createOneTime(ScheduleParameters.END);
-		schedule.schedule(params, this, "endExperiment");
-	}
-	
-	public static void stopSim() {
-		RunEnvironment.getInstance().getCurrentSchedule().setFinishing(true);
-	}
-	
-	public void endExperiment() {
-		InprogressExperiment.end();
 	}
 }
