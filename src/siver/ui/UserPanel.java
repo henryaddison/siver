e8b394e010d10a9261d36996dae635774db45298
Henry Addison <hja22@hja22.co.uk>
2012-08-10 12:25:04 +0100

    Removed warnings from UserPanel by making it clear that variables used to hold cox brain classes should extend from CoxBrain

--- src/siver/ui/UserPanel.java
+++ src/siver/ui/UserPanel.java
@@ -6,14 +6,15 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import siver.boat.*;
 import siver.context.SiverContextCreator;
 import siver.cox.brains.BasicBrain;
+import siver.cox.brains.CoxBrain;
 import siver.cox.brains.LaneChangeBrain;
 import siver.cox.brains.StartStopBrain;
-import siver.river.BoatHouse;
 
 public class UserPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
 	public UserPanel()  {
 		initComponents();
 	}
@@ -24,14 +25,14 @@ public class UserPanel extends JPanel {
 		
 		setBackground(new java.awt.Color(255, 0, 0));
 		
-		ArrayList<Class> coxBrainClassesNames = new ArrayList<Class>();
+		ArrayList<Class<? extends CoxBrain>> coxBrainClassesNames = new ArrayList<Class<? extends CoxBrain>>();
 		coxBrainClassesNames.add(BasicBrain.class);
 		coxBrainClassesNames.add(StartStopBrain.class);
 		coxBrainClassesNames.add(LaneChangeBrain.class);
 		
 		
-		for(Class klass : coxBrainClassesNames) {
-			final Class coxBrainClass = klass;
+		for(Class<? extends CoxBrain> klass : coxBrainClassesNames) {
+			final Class<? extends CoxBrain> coxBrainClass = klass;
 			
 			JButton launchButton = new JButton();
 			
@@ -47,7 +48,7 @@ public class UserPanel extends JPanel {
 		}
 	}
 	
-	private void launchButtonActionPerformed(java.awt.event.ActionEvent evt, Class coxBrainClass) {
+	private void launchButtonActionPerformed(java.awt.event.ActionEvent evt, Class<? extends CoxBrain> coxBrainClass) {
 		SiverContextCreator.getBoatHouse().manualLaunch(coxBrainClass);
 	}
 }
