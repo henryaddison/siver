a0710a8a27e3951dfe21a340a1b827a9d1fa03ac
Henry Addison <hja22@hja22.co.uk>
2012-08-20 12:05:35 +0100

    Added an overtaking brain with observations to go with it

--- src/siver/cox/CoxObservations.java
+++ src/siver/cox/CoxObservations.java
@@ -2,6 +2,8 @@ package siver.cox;
 
 import siver.boat.Boat;
 import siver.boat.BoatNavigation;
+import siver.river.River.NoLaneFound;
+import siver.river.lane.Lane;
 import siver.river.lane.LaneEdge;
 import siver.river.lane.LaneNode;
 
@@ -10,6 +12,7 @@ public class CoxObservations {
 	private Boat boat;
 	private BoatNavigation navigator;
 	private static final int VIEWING_DISTANCE = 3;
+	private static final double OVERTAKING_SPEED_DIFFERENCE = 1.0;
 	
 	public CoxObservations(Cox cox, Boat boat, BoatNavigation navigator) {
 		this.cox = cox;
@@ -27,26 +30,83 @@ public class CoxObservations {
 		return boat.getGear() < cox.desired_gear();
 	}
 	
+	public boolean aboveDesiredSpeed() {
+		return boat.getGear() > cox.desired_gear();
+	}
+	
 	public int boatGear() {
 		return boat.getGear();
 	}
 	
 	public boolean nearbyBoatInfront() {
-		LaneNode node = navigator.getDestinationNode();
-		boolean upstream = navigator.headingUpstream();
+		return (nearbyOccupiedEdge(navigator.getDestinationNode(), navigator.headingUpstream()) != null);
+	}
+	
+	private LaneEdge nearbyOccupiedEdge(LaneNode node, boolean infront) {
 		int edges_ahead = 1;
 		while(edges_ahead <= VIEWING_DISTANCE) {
-			LaneEdge edge = node.getLane().getNextEdge(node, upstream);
+			LaneEdge edge = node.getLane().getNextEdge(node, infront);
 			//if there is no further edge then there is no boat in front in viewing distance (though there is the end of the river)
-			if(edge == null) return false;
+			if(edge == null) return null;
 			//as soon as we find an empty edge in front we can return false
-			if(!edge.isEmpty()) return true;
+			if(!edge.isEmpty()) return edge;
 			//otherwise move on to next edge
-			node = edge.getNextNode(upstream);
+			node = edge.getNextNode(infront);
 			edges_ahead++;
 		}
+		return null;
+	}
+	
+	public boolean slowBoatInfront() {
+		if(!nearbyBoatInfront()) {
+			// return false is there is no boat at all in front
+			return false;
+		}
 		
+		LaneEdge occupiedEdgeAhead = nearbyOccupiedEdge(navigator.getDestinationNode(), navigator.headingUpstream());
+		
+		for(Cox coxInfront : occupiedEdgeAhead.getCoxes()) {			
+			if(cox.getNavigator().headingUpstream() == coxInfront.getNavigator().headingUpstream()) {
+				//if boats are travelling in the same direction then want the difference in their speeds to be greater than limit
+				return (Math.abs(boat.getSpeed() - coxInfront.getBoat().getSpeed()) > OVERTAKING_SPEED_DIFFERENCE);
+			} else {
+				//if boats are travelling in the different direction then want the sum of their speeds to be greater than limit
+				return (Math.abs(boat.getSpeed()) + Math.abs(coxInfront.getBoat().getSpeed()) > OVERTAKING_SPEED_DIFFERENCE);
+			}
+			
+		}
 		return false;
+	}
+	
+	public boolean laneToLeftIsClear() {
+		try {
+			Lane laneToLeft = boat.getRiver().getLaneToLeftOf(navigator.getLane(), navigator.headingUpstream());
+			return laneIsClear(laneToLeft);
+		} catch (NoLaneFound e) {
+			//if there is no lane to the left, then it is not clear
+			return false;
+		}
+	}
+	
+	public boolean laneToRightIsClear() {
+		try {
+			Lane laneToRight = boat.getRiver().getLaneToRightOf(navigator.getLane(), navigator.headingUpstream());
+			return laneIsClear(laneToRight);
+		} catch (NoLaneFound e) {
+			//if there is no lane to the left, then it is not clear
+			return false;
+		}
+	}
+	
+	private boolean laneIsClear(Lane lane) {
+		LaneNode nearestNodeInLane = lane.nodeNearest(boat.getLocation());
+		//return true only if there's nothing ahead and nothing behind
+		//very simplistic at the moment, only checks existence, not relative speed
+		return (nearbyOccupiedEdge(nearestNodeInLane, true) == null) && (nearbyOccupiedEdge(nearestNodeInLane, false) == null);
+	}
+	
+	public boolean changingLane() {
+		return navigator.changingLane();
 	}
 	
 	public boolean outingOver() {
---
+++ src/siver/cox/brains/OvertakingBrain.java
@@ -0,0 +1,48 @@
+package siver.cox.brains;
+
+import siver.cox.actions.*;
+
+public class OvertakingBrain extends CoxBrain {
+	private boolean overtaking = false;
+	
+	
+	@Override
+	protected Class<? extends Action> typeSpecificActionChoice() {
+		if(latestObservations.atRiversEnd()) {
+			overtaking = false;
+			return Spin.class;
+		}
+		if(overtaking) {
+			return overtakingActionChoice();
+		}
+		if(latestObservations.aboveDesiredSpeed()) {
+			return SlowDown.class;
+		}
+		if(latestObservations.slowBoatInfront() && latestObservations.laneToLeftIsClear()) {
+			overtaking = true;
+			return MoveToLaneOnLeft.class;
+		}
+		if(latestObservations.nearbyBoatInfront()) {
+			return SlowDown.class;
+		}
+		if(latestObservations.belowDesiredSpeed()) {
+			return SpeedUp.class;
+		}
+		if(true) {
+			return LetBoatRun.class;
+		}
+		throw new RuntimeException("No action chosen by brain. Something has gone very wrong");
+	}
+	
+	private Class<? extends Action> overtakingActionChoice() {
+		if(latestObservations.laneToRightIsClear() && !latestObservations.changingLane()) {
+			overtaking = false;
+			return MoveToLaneOnRight.class;
+		}
+		if(true) {
+			return SpeedUp.class;
+		}
+		throw new RuntimeException("No action chosen by brain. Something has gone very wrong");
+	}
+
+}
