f6e3dd6f37728f0c90bbfab2b7c19653789d9472
Henry Addison <hja22@hja22.co.uk>
2012-08-20 19:45:33 +0100

    improved delay vs crashes code to be more readible

--- plot_delay_vs_crashes.R
+++ plot_delay_vs_crashes.R
@@ -1,13 +1,10 @@
 library(RMySQL)
 con <- dbConnect("MySQL", group = "siver_development")
 
-
-query = "select experiment_runs.brain_type,
-sum(experiment_runs.crash_count)/count(distinct experiment_runs.id) as avg_crashes_per_run,
+query = "select 
+sum(experiment_runs.crash_count)/count(distinct experiment_runs.id) as ycol,
 count(boat_records.id)/count(distinct experiment_runs.id) as boats_launched,
-max(boat_records.launch_tick)/(count(boat_records.id)/count(distinct experiment_runs.id) - 1) as delay,
-sum(if(boat_records.land_tick IS NOT NULL, 1, 0))/count(distinct experiment_runs.id) as boats_landed,
-schedules.name
+max(boat_records.launch_tick)/(count(boat_records.id)/count(distinct experiment_runs.id) - 1) as xcol
 from experiment_runs
 join boat_records on boat_records.experiment_run_id = experiment_runs.id
 join experiments on experiments.id = experiment_runs.experiment_id
@@ -16,13 +13,14 @@ where
 experiment_runs.brain_type != 'RandomChoice'
 and experiment_runs.brain_type = '%s'
 group by schedules.name, experiment_runs.brain_type
-having boats_launched = %d"
+having boats_launched = %d
+ORDER BY xcol"
 launched <- 10
 xlab='Delay between launched'
 ylab='Average number of crashes'
-axes <- T;
 
 
+plot(list(), list(), ylim=c(0,3000), xlim=c(0,600), xlab=xlab, ylab=ylab)
 
 brain_types <- c("BasicBrain", "ConservativeBrain", "OvertakingBrain", "RandomMovement")
 
@@ -36,14 +34,7 @@ for (brain_type in brain_types) {
 	
 	brain_data = fetch(res, -1)
 
-	plot(x=brain_data$delay, y=brain_data$avg_crashes_per_run, xlab=xlab, ylab=ylab, axes=axes, ylim=c(0,3000), col=brain_types_to_colour_map[brain_type])
-	axes <- F
-	xlab <- ''
-	ylab <- ''
-	par(new=T)
+	points(x=brain_data$xcol, y=brain_data$ycol, col=brain_types_to_colour_map[brain_type])
 }
 
-
-
-par(new=F)
 dbDisconnect(con)
