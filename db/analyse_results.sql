62a51014dbd5f7a21aae452c25d12fada6db127e
Henry Addison <hja22@hja22.co.uk>
2012-08-20 19:47:05 +0100

    new column to analyse results - this query is largely being superceded by R scripts

--- db/analyse_results.sql
+++ db/analyse_results.sql
@@ -4,6 +4,7 @@ sum(experiment_runs.crash_count)/count(distinct experiment_runs.id) as avg_crash
 count(boat_records.id)/count(distinct experiment_runs.id) as boats_launched,
 max(boat_records.launch_tick)/(count(boat_records.id)/count(distinct experiment_runs.id) - 1) as delay,
 sum(if(boat_records.land_tick IS NOT NULL, 1, 0))/count(distinct experiment_runs.id) as boats_landed,
+sum(if(boat_records.land_tick IS NOT NULL, boat_records.aggregate_gear_difference, 0))/sum(if(boat_records.land_tick IS NOT NULL, 1, 0)) as avg_landed_boat_gear_difference,
 schedules.name
 from experiment_runs
 join boat_records on boat_records.experiment_run_id = experiment_runs.id
@@ -11,6 +12,7 @@ join experiments on experiments.id = experiment_runs.experiment_id
 join schedules on schedules.id = experiments.schedule_id
 where
 experiment_runs.brain_type != 'RandomChoice'
+#and experiment_runs.brain_type = 'BasicBrain'
 group by schedules.name, experiment_runs.brain_type
 #order by experiment_runs.brain_type, schedules.name
-#having boats_launched = 10 and delay = 600
+having boats_launched = 10
