f788b2d7746605bcf6cf694e2a90f96c82513ebb
Henry Addison <hja22@hja22.co.uk>
2012-08-21 15:13:10 +0100

    new, more careful versions of analysis queries

--- db/analysis/sql/analyse_results.sql
+++ db/analysis/sql/analyse_results.sql
@@ -1,18 +1,22 @@
-select 
-experiment_runs.brain_type,
-sum(experiment_runs.crash_count)/count(distinct experiment_runs.id) as avg_crashes_per_run,
-count(boat_records.id)/count(distinct experiment_runs.id) as boats_launched,
-max(boat_records.launch_tick)/(count(boat_records.id)/count(distinct experiment_runs.id) - 1) as delay,
-sum(if(boat_records.land_tick IS NOT NULL, 1, 0))/count(distinct experiment_runs.id) as boats_landed,
-sum(if(boat_records.land_tick IS NOT NULL, boat_records.aggregate_gear_difference, 0))/sum(if(boat_records.land_tick IS NOT NULL, 1, 0)) as avg_landed_boat_gear_difference,
-schedules.name
-from experiment_runs
-join boat_records on boat_records.experiment_run_id = experiment_runs.id
-join experiments on experiments.id = experiment_runs.experiment_id
-join schedules on schedules.id = experiments.schedule_id
-where
-experiment_runs.brain_type != 'RandomChoice'
-#and experiment_runs.brain_type = 'BasicBrain'
-group by schedules.name, experiment_runs.brain_type
-#order by experiment_runs.brain_type, schedules.name
-having boats_launched = 10
+SELECT
+  schedules.name,
+  experiment_runs.brain_type,
+  boats_launched,
+  AVG(boats_landed),
+  AVG(experiment_runs.crash_count) as ycol,
+  delay as xcol
+FROM experiment_runs
+JOIN (SELECT 
+  experiment_run_id,
+  COUNT(boat_records.id) as boats_launched,
+  SUM(IF(boat_records.land_tick IS NOT NULL, 1, 0)) as boats_landed,
+  MAX(boat_records.launch_tick)/(COUNT(boat_records.id)-1) as delay
+  FROM boat_records
+  GROUP BY experiment_run_id
+) AS boat_schedule ON boat_schedule.experiment_run_id = experiment_runs.id
+JOIN experiments ON experiments.id = experiment_runs.experiment_id
+JOIN schedules ON schedules.id = experiments.schedule_id
+WHERE boats_launched = 10
+AND experiment_runs.brain_type = "BasicBrain"
+GROUP BY boats_launched, delay, experiment_runs.brain_type
+ORDER BY experiment_runs.brain_type
---
+++ db/analysis/sql/percent_boats_landed.sql
@@ -0,0 +1,19 @@
+SELECT
+experiment_runs.brain_type,
+AVG(boats_landed/boats_launched*100) as percent_boats_returned
+FROM (
+SELECT 
+  experiment_run_id,
+  COUNT(boat_records.id) as boats_launched,
+  SUM(IF(boat_records.land_tick IS NOT NULL, 1, 0)) as boats_landed,
+  MAX(boat_records.launch_tick)/(COUNT(boat_records.id)-1) as delay
+  FROM boat_records
+  GROUP BY experiment_run_id
+) AS aggregate_boat_records
+JOIN experiment_runs ON experiment_runs.id = aggregate_boat_records.experiment_run_id
+JOIN experiments ON experiments.id = experiment_runs.experiment_id
+JOIN schedules ON schedules.id = experiments.schedule_id
+#WHERE experiment_runs.brain_type = 'BasicBrain'
+
+GROUP BY experiment_runs.brain_type
+ORDER BY experiment_runs.brain_type, boats_launched, schedules.name, schedules.version
