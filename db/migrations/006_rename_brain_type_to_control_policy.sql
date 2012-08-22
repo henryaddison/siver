033e9026dc13ef5a31f54f9006f9648dad2ee1d1
Henry Addison <hja22@hja22.co.uk>
2012-08-22 20:30:32 +0100

    Fixed migration to allow null control policies for records e.g. when not running automatically

--- db/migrations/006_rename_brain_type_to_control_policy.sql
+++ db/migrations/006_rename_brain_type_to_control_policy.sql
@@ -6,9 +6,9 @@ ALTER TABLE simulation_parameters
 CHANGE brain_type control_policy VARCHAR(255) NOT NULL;
 
 ALTER TABLE simulation_runs
-CHANGE brain_type control_policy VARCHAR(255) NOT NULL;
+CHANGE brain_type control_policy VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE boat_records
-CHANGE brain_type control_policy VARCHAR(255) NOT NULL;
+CHANGE brain_type control_policy VARCHAR(255) DEFAULT NULL;
 
 INSERT INTO migrations VALUES (6);
