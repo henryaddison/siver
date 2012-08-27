5983537b9d845b6fc32fcbd5d252cc188d53b559
Henry Addison <hja22@hja22.co.uk>
2012-08-27 17:22:57 +0100

    Added rounding methods to single_boats.sql for more human readible output

--- db/analysis/sql/single_boats.sql
+++ db/analysis/sql/single_boats.sql
@@ -1,10 +1,9 @@
 select
-simulation_runs.control_policy,
-avg(boat_records.launch_tick),
-avg(boat_records.land_tick),
-avg(boat_records.desired_gear),
-avg(boat_records.distance_covered),
-avg(boat_records.aggregate_gear_difference)
+simulation_runs.control_policy as control_policy,
+ROUND(avg(boat_records.land_tick),0) as avg_land_tick,
+ROUND(avg(boat_records.desired_gear),0) as desired_gear,
+avg(boat_records.distance_covered) as avg_distance_covered,
+ROUND(avg(boat_records.aggregate_gear_difference),0) as avg_aggregate_gear_difference
 from 
 simulation_runs 
 join simulation_parameters on simulation_parameters.id = simulation_runs.simulation_parameters_id
