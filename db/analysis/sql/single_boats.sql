1e9a313fb58479daa813cbc53de865426dfa7db0
Henry Addison <hja22@hja22.co.uk>
2012-08-31 14:54:08 +0100

    Capitalized the SQL keywords in single_boat.sql to it looks nicer in print

--- db/analysis/sql/single_boats.sql
+++ db/analysis/sql/single_boats.sql
@@ -1,14 +1,14 @@
-select
+SELECT
 simulation_runs.control_policy as control_policy,
-ROUND(avg(boat_records.land_tick),0) as avg_land_tick,
-ROUND(avg(boat_records.desired_gear),0) as desired_gear,
-avg(boat_records.distance_covered) as avg_distance_covered,
-ROUND(avg(boat_records.aggregate_gear_difference),0) as avg_aggregate_gear_difference
-from 
+ROUND(AVG(boat_records.land_tick),0) as avg_land_tick,
+ROUND(AVG(boat_records.desired_gear),0) as desired_gear,
+AVG(boat_records.distance_covered) as avg_distance_covered,
+ROUND(AVG(boat_records.aggregate_gear_difference),0) as avg_aggregate_gear_difference
+FROM 
 simulation_runs 
-join simulation_parameters on simulation_parameters.id = simulation_runs.simulation_parameters_id
-join schedules on schedules.id = simulation_parameters.schedule_id
-join boat_records on simulation_runs.id = boat_records.simulation_run_id
-where simulation_runs.flushed = 1
+JOIN simulation_parameters ON simulation_parameters.id = simulation_runs.simulation_parameters_id
+JOIN schedules ON schedules.id = simulation_parameters.schedule_id
+JOIN boat_records ON simulation_runs.id = boat_records.simulation_run_id
+WHERE simulation_runs.flushed = 1
 AND schedules.name = "One boat"
-group by simulation_runs.control_policy
+GROUP BY simulation_runs.CONTROL_POLICY
