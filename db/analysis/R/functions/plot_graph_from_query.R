aac77e0d1f79a3116c2b656039d2f78ad853350d
Henry Addison <hja22@hja22.co.uk>
2012-09-06 16:35:05 +0100

    Plotting monochrome graphs with different line and point types

--- db/analysis/R/functions/plot_graph_from_query.R
+++ db/analysis/R/functions/plot_graph_from_query.R
@@ -3,7 +3,13 @@ plot_graph_from_query <- function(query, xlab, ylab, xlim, ylim, main_title) {
 	con <- dbConnect("MySQL", user="siver", host="localhost", dbname=dbname)
 	control_policies <- c("GearFocussed", "SafetyFocussed", "Overtaking", "RandomMovement")
 
-	control_policies_to_colour_map = c(GearFocussed="black", Overtaking="green", RandomMovement="blue", SafetyFocussed="red")
+  # control_policies_to_colour_map = c(GearFocussed="black", Overtaking="green", RandomMovement="blue", SafetyFocussed="red")
+	
+	control_policies_to_colour_map = c(GearFocussed="black", Overtaking="black", RandomMovement="black", SafetyFocussed="black")
+	
+	cp_to_lty = c(GearFocussed=1, Overtaking=2, RandomMovement=3, SafetyFocussed=4)
+
+  cp_to_pch = c(GearFocussed=0, Overtaking=4, RandomMovement=2, SafetyFocussed=1)
 	
 	launched_options <- c(10, 20, 30)
 	
@@ -17,7 +23,7 @@ plot_graph_from_query <- function(query, xlab, ylab, xlim, ylim, main_title) {
 		plot(list(), list(), xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, xaxs = "i", yaxs = "i")
 		
 		title(main=paste(strwrap(interpolated_main_title, width=60), collapse="\n"))
-		legend("topright", inset=.05, title="Control Policy", control_policies, col=control_policies_to_colour_map[control_policies], pch=1)
+    legend("topright", inset=.05, title="Control Policy", control_policies, col=control_policies_to_colour_map[control_policies], pch=cp_to_pch[control_policies], lty=cp_to_lty[control_policies])
 		
 		for (control_policy in control_policies) {
 			interpolated_query = sprintf(query, control_policy, launched)
@@ -26,7 +32,7 @@ plot_graph_from_query <- function(query, xlab, ylab, xlim, ylim, main_title) {
 		
 			brain_data = fetch(res, -1)
 	
-			points(x=brain_data$xcol, y=brain_data$ycol, col=control_policies_to_colour_map[control_policy], type='o')
+			points(x=brain_data$xcol, y=brain_data$ycol, col=control_policies_to_colour_map[control_policy], type='o', pch=cp_to_pch[control_policy], lty=cp_to_lty[control_policy])
 		}
 		
 		dev.off()
